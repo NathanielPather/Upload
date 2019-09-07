5069897d97886f01ee9d4fac16e9e59d4dd5c6b8
Nathaniel Pather <nathaniel_pather@hotmail.com>
2019-09-07 21:28:15 +1000

    Added cors configuration to express server.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const server = express()
 const cors = require('cors')
 
+var corsOptions = {
+	origin: '*',
+	optionsSuccessStatus: 200,
+}
+
+server.use(cors(corsOptions))
+
 server.listen(8000, () => {
 	console.log('Server started!')
 })
